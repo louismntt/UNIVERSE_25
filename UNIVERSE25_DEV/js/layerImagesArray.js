9a557f37594010d011323f9d4556cb2294cf793a
Louis Manotte <92359003+louismntt@users.noreply.github.com>
2022-06-02 22:13:41 +0200

    upload of repo

---
+++ UNIVERSE25_DEV/js/layerImagesArray.js
@@ -0,0 +1,23 @@
+const layer1ImagesArray = [
+  ["Layer1.png", "layer bouuu"],
+  ["Layer2.png", "layer rhooo"],
+  ["Layer3.png", "layer yeeee"],
+]
+
+const layer2ImagesArray = [
+  ["MidLayer1.png", "layer bouuu"],
+  ["MidLayer2.png", "layer rhooo"],
+  ["MidLayer3.png", "layer yeeee"],
+]
+
+const layer3ImagesArray = [
+  ["TextLayer1.png", "layer bouuu"],
+  ["TextLayer2.png", "layer rhooo"],
+  ["TextLayer3.png", "layer yeeee"],
+]
+
+const layer4ImagesArray = [
+  ["TopLayer1.png", "layer bouuu"],
+  ["TopLayer2.png", "layer rhooo"],
+  ["TopLayer3.png", "layer yeeee"],
+]
