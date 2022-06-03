4bf4b52e9f2006b7de39a4039fcc6eba8c90d615
Louis Manotte <92359003+louismntt@users.noreply.github.com>
2022-06-03 15:48:14 +0200

    Commit new version

---
+++ js/layerImagesArray.js
@@ -0,0 +1,61 @@
+const activeLayer0 = "POD_banner_concept.jpg";
+
+const layer1ImagesArray = [
+  ["Layer1.png", "layer bouuu"],
+  ["Layer2.png", "layer rhooo"],
+  ["Layer3.png", "layer yeeee"],
+  ["Layer1.png", "layer bouuu"],
+  ["Layer2.png", "layer rhooo"],
+  ["Layer3.png", "layer yeeee"],
+  ["Layer1.png", "layer bouuu"],
+  ["Layer2.png", "layer rhooo"],
+  ["Layer3.png", "layer yeeee"],
+  ["Layer1.png", "layer bouuu"],
+  ["Layer2.png", "layer rhooo"],
+  ["Layer3.png", "layer yeeee"],
+]
+
+const layer2ImagesArray = [
+  ["MidLayer1.png", "layer bouuu"],
+  ["MidLayer2.png", "layer rhooo"],
+  ["MidLayer3.png", "layer yeeee"],
+  ["MidLayer1.png", "layer bouuu"],
+  ["MidLayer2.png", "layer rhooo"],
+  ["MidLayer3.png", "layer yeeee"],
+  ["MidLayer1.png", "layer bouuu"],
+  ["MidLayer2.png", "layer rhooo"],
+  ["MidLayer3.png", "layer yeeee"],
+  ["MidLayer1.png", "layer bouuu"],
+  ["MidLayer2.png", "layer rhooo"],
+  ["MidLayer3.png", "layer yeeee"],
+]
+
+const layer3ImagesArray = [
+  ["TextLayer1.png", "layer bouuu"],
+  ["TextLayer2.png", "layer rhooo"],
+  ["TextLayer3.png", "layer yeeee"],
+  ["TextLayer1.png", "layer bouuu"],
+  ["TextLayer2.png", "layer rhooo"],
+  ["TextLayer3.png", "layer yeeee"],
+  ["TextLayer1.png", "layer bouuu"],
+  ["TextLayer2.png", "layer rhooo"],
+  ["TextLayer3.png", "layer yeeee"],
+  ["TextLayer1.png", "layer bouuu"],
+  ["TextLayer2.png", "layer rhooo"],
+  ["TextLayer3.png", "layer yeeee"],
+]
+
+const layer4ImagesArray = [
+  ["TopLayer1.png", "layer bouuu"],
+  ["TopLayer2.png", "layer rhooo"],
+  ["TopLayer3.png", "layer yeeee"],
+  ["TopLayer1.png", "layer bouuu"],
+  ["TopLayer2.png", "layer rhooo"],
+  ["TopLayer3.png", "layer yeeee"],
+  ["TopLayer1.png", "layer bouuu"],
+  ["TopLayer2.png", "layer rhooo"],
+  ["TopLayer3.png", "layer yeeee"],
+  ["TopLayer1.png", "layer bouuu"],
+  ["TopLayer2.png", "layer rhooo"],
+  ["TopLayer3.png", "layer yeeee"],
+]
